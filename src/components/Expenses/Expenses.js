c511eea4d1d428759d0bc6be7b2a01eee7c3ed58
Arek Lechnik <alechnik@gmail.com>
2023-02-24 16:40:56 +0100

    Create ExpensesFilter components + adding logic to components +  css

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,11 +1,25 @@
+import { useState } from "react";
+
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 
 import "./Expenses.css";
 
 const Expenses = (props) => {
+	const curentDate = new Date();
+	const [filteredYear, setFilteredYear] = useState(
+		curentDate.getFullYear(toString)
+	);
+	const handleFilterChnge = (selectedYrar) => {
+		setFilteredYear(selectedYrar);
+	};
 	return (
 		<Card className='expenses'>
+			<ExpensesFilter
+				selected={filteredYear}
+				onChangeFilter={handleFilterChnge}
+			/>
 			<ExpenseItem
 				amount={props.expenses[0].amount}
 				date={props.expenses[0].date}
